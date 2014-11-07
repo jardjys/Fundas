e2bbe43aed91e977c1af9cb86deb86fc8aabf92a
jardjys <jardjys@hotmail.com>
2014-11-07 08:55:33 -0600

    js

--- js/index.js
+++ js/index.js
@@ -1,8 +1,4 @@
- $(document).ready(function(){
-
-    $(".dropdown-menu").on('click', 'li a', function(){
-      $(".btn-primary:first-child").text($(this).text());
-      $(".btn-primary:first-child").val($(this).text());
-   });
-
+$(".dropdown-menu li a").click(function(){
+  $(this).parents(".input-group-btn").find('.btn').text($(this).text());
+  $(this).parents(".input-group-btn").find('.btn').val($(this).text());
 });
